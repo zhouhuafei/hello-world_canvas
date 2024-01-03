919f5d38a435503f916f40cc1d28408362a697dc
zhouhuafei <1123486116@qq.com>
2024-01-03 13:53:20 +0800

    feat: 🎸 rect

--- src/views/canvas/1/index.ts
+++ src/views/canvas/1/index.ts
@@ -1,5 +1,8 @@
 class CanvasDraw {
   options = { wrap: '.canvas-wrap' }
+  canvasWrap
+  canvas
+  ctx
 
   constructor (options: any = {}) {
     Object.assign(this.options, options)
@@ -8,6 +11,32 @@ class CanvasDraw {
   }
 
   init () {
+    this.genCanvas()
+    this.genCtx()
+    this.draw()
+  }
+
+  genCanvas () {
+    this.canvasWrap = document.querySelector(this.options.wrap)
+    this.canvas = document.createElement('canvas')
+    const { clientWidth, clientHeight } = document.documentElement
+    this.canvas.width = clientWidth
+    this.canvas.height = clientHeight
+    this.canvas.style.display = 'block'
+    this.canvasWrap.appendChild(this.canvas)
+  }
+
+  genCtx () {
+    this.ctx = this.canvas.getContext('2d')
+  }
+
+  draw () {
+    this.drawReact()
+  }
+
+  drawReact () {
+    this.ctx.rect(0, 0, 100, 100)
+    this.ctx.fill()
   }
 }
 
