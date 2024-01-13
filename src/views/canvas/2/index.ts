a7d66536004150f3f750357990a7af100a475de6
zhouhuafei <1123486116@qq.com>
2024-01-13 15:53:20 +0800

    feat: 🎸 水视频第06期

--- src/views/canvas/2/index.ts
+++ src/views/canvas/2/index.ts
@@ -1,27 +1,9 @@
-import { CanvasDrawMain } from '@/views/canvas/1/utils/CanvasDrawMain'
+import { CanvasDrawMain } from '@/views/canvas/2/utils/CanvasDrawMain'
 
 class CanvasDrawPage {
-  constructor (options: any = {}) {
+  constructor () {
     new CanvasDrawMain({
-      ...options,
-      mainBgImageUrl: new URL('./images/mainBg.png', import.meta.url).href,
-      mainBgAudioUrl: new URL('./audios/mainBg.mp3', import.meta.url).href,
-      mainTexts: [
-        `宠妻者 风生水起`,
-        `亏妻者 百财不入`
-      ],
-      maskBg1ImageUrl: new URL('./images/maskBg1.png', import.meta.url).href,
-      maskBg2ImageUrl: new URL('./images/maskBg2.png', import.meta.url).href,
-      maskBg3ImageUrl: new URL('./images/maskBg3.png', import.meta.url).href,
-      maskBg4ImageUrl: new URL('./images/maskBg4.png', import.meta.url).href,
-      maskBg5ImageUrl: new URL('./images/maskBg5.png', import.meta.url).href,
-      maskBg6ImageUrl: new URL('./images/maskBg6.png', import.meta.url).href,
-      maskBg7ImageUrl: new URL('./images/maskBg7.png', import.meta.url).href,
-      maskBg8ImageUrl: new URL('./images/maskBg8.png', import.meta.url).href,
-      maskBg9ImageUrl: new URL('./images/maskBg9.png', import.meta.url).href,
-      maskBg10ImageUrl: new URL('./images/maskBg10.png', import.meta.url).href,
-      maskBg11ImageUrl: new URL('./images/maskBg11.png', import.meta.url).href,
-      maskNum: 11
+      mainBgAudioUrl: new URL('./audios/mainBg.mp3', import.meta.url).href
     })
   }
 }
