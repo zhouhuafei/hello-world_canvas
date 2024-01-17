64168f5fb7ede4c70a3ee9a3d25d7028ebbdf071
zhouhuafei <1123486116@qq.com>
2024-01-17 22:32:30 +0800

    feat: 🎸 16比9的分辨率：一行展示30个字效果最佳

--- src/views/canvas/0/utils/CanvasDrawMain.ts
+++ src/views/canvas/0/utils/CanvasDrawMain.ts
@@ -122,7 +122,7 @@ class CanvasDrawMain {
     const obj: any = {}
     let allLineHeight = 0
     this.options.mainTexts.forEach((text, index) => {
-      const prefectWordLength = 28 // 16比9的分辨率：一行展示28个字效果最佳
+      const prefectWordLength = 30 // 16比9的分辨率：一行展示30个字效果最佳
       obj[`fontSize${index}`] = (this.width - this.padding * 2) / prefectWordLength
       obj[`lineHeight${index}`] = obj[`fontSize${index}`] * 1.6
       allLineHeight += obj[`lineHeight${index}`]
